b2fd476d869b2e9de942ed322dd5e87f7721dedb
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 17:00:39 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// IF STATEMENT REVIEW
+// Day of the Week: Prompt the user to enter a day of the week (e.g., "Monday") and say whether it's a weekday or the weekend.
+let user = prompt("What's the day of the week?")
+
+if(user === "Saturday" || user === "Sunday"){
+  console.log("It's the weekend, yay!!!")
+} else {
+  console.log("It's a week day, yay school!!!!!")
+}
+
+
+// Age Group Classifier: Prompt the user to enter their age and categorize them into different age groups (child, teen, adult, senior)
+let age = prompt("What is your age")
+age = parseInt(age)
+
+if (age < 0){
+  console.log("That's not your age")
+}else if (age <= 12){
+  console.log("You are a child")
+}else if(age<=18){
+  console.log("You are a teen")
+}else{
+  console.log("RIP you lived a good life")
+}
+
+
+
+// WHILE LOOPS
+// Print 1 to 5
+let i = 1
+
+// Print from a certain number to 1
+
+
+
+
+// Summing up to 5
+
+
+
+
+// Using prompt
+
+
+
+
